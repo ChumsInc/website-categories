b075456e639735d4aa81d70a5200de8f743fa676
Steve Montgomery <steve@chums.com>
2022-06-06 08:56:15 -0600

    chore: update name of project, npm updates

--- webpack.common.js
+++ webpack.common.js
@@ -36,6 +36,13 @@ module.exports = {
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
+        fallback: {
+            // fix for:
+            // ERROR in ./node_modules/react-dnd/dist/core/DndProvider.js 28:0-48
+            // Module not found: Error: Can't resolve 'react/jsx-runtime' in 'C:\Users\steve\PhpstormProjects\website-categories\node_modules\react-dnd\dist\core'
+            'react/jsx-runtime': 'react/jsx-runtime.js',
+            'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+        },
     },
     optimization: {
         splitChunks: {
